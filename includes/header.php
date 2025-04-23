00e445f8aad252438ffd7aa02eece7e5d6574bee
Blue <Bluegamerdogyt@gmail.com>
2025-04-23 12:31:10 +0200

    Header & Footer hotfix

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Mein Blog</a>
+            <a class="navbar-brand" href="#">IT Forum</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -23,11 +23,11 @@
                         <a href="index.php" class="nav-link active" aria-current="page">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="post_insert.php" class="nav-link active">Neuer Beitrag</a>
+                        <a href="create_post.php" class="nav-link active">Neuer Beitrag</a>
                     </li>
                     <?php if (isset($_SESSION['admin'])): ?>
                         <li class="nav-item">
-                            <a href="post_delete.php" class="nav-link active">Beiträge Verwalten</a>
+                            <a href="delete_post.php" class="nav-link active">Beiträge Verwalten</a>
                         </li>
                     <?php endif; ?>
                 </ul>
@@ -37,7 +37,7 @@
                         <?= isset($_SESSION['admin']) ? htmlspecialchars($_SESSION['admin']) : 'Gast' ?>
                     </span>
                     <?php if (isset($_SESSION['admin'])): ?>
-                        <form action="login.php" method="post" class="d-inline">
+                        <form action="logout.php" method="post" class="d-inline">
                             <button type="submit" name="logout" class="btn btn-outline-dark btn-sm">Logout</button>
                         </form>
                     <?php else: ?>
