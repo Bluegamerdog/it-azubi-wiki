ef4dc9a19bdfe88b56bacc1c0fafb3474c708b44
Blue <Bluegamerdogyt@gmail.com>
2025-05-02 14:03:46 +0200

    Implemented logo

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,8 @@ verifyLoginState($pdo);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" defer></script>
     <script src="assets/js/darkmode.js" defer></script>
 
-    <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+    <!-- <link rel="icon" href="assets/favicon.ico" type="image/x-icon"> -->
+    <link rel="icon" href="assets/logo.png" type="image/png">
 
     <style>
         /* Custom sidebar styles */
@@ -128,6 +129,11 @@ verifyLoginState($pdo);
             <button class="btn btn-outline-secondary d-lg-none me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas">
                 ☰
             </button>
+
+            <a class="navbar-brand" href="<?= htmlspecialchars($pageHref ?? "index.php") ?>">
+                <img src="assets/logo.png" alt="Website Logo" class="img-fluid" style="max-height: 40px;">
+            </a>
+
 
             <a class="navbar-brand" href="<?= htmlspecialchars($pageHref ?? "index.php") ?>"><?= htmlspecialchars($pageHeader ?? "IT Forum") ?></a>
 
